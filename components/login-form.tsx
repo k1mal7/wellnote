52bd09240c08cee33180c72071959e0e938632b7
N <admin@mac.home>
2026-08-26 19:25:19 -0400

    Make beta invite only

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -115,15 +115,7 @@ window.location.assign("/protected");
               </Button>
             </div>
 
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <Link
-                href="/auth/sign-up"
-                className="underline underline-offset-4"
-              >
-                Sign up
-              </Link>
-            </div>
+            
           </form>
         </CardContent>
       </Card>
